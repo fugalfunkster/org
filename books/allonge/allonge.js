24dc059e4157837fdfd09273533a1636e445b63f
Matthew Hoselton <mtthwhsltn@gmail.com>
2016-04-18 23:55:55 -0700

    recover "make data w/ funcs" & tangle allonge.js

--- books/allonge/allonge.js
+++ books/allonge/allonge.js
@@ -1,78 +1,122 @@
 const callFirst = (fn, larg) =>
-  function (rest) {
+  function(rest) {
   return fn.call(this, larg, rest);
 };
 
 const callLast = (fn, rarg) =>
-  function (rest) {
+  function(rest) {
   return fn.call(this, rest, rarg);
 };
 
+const callLeft = (fn, ...args) =>
+  (remainingArgs) =>
+    fn(...args, ...remainingArgs);
+
+const callRight = (fn, ...args) =>
+ (remainingArgs) =>
+   fn(remainingArgs, args);
+
 const unary = (fn) =>
-   fn.length === 1
-     ? fn
-     : function (something) {
-         return fn.call(this, something);
-};
+   fn.length === 1 ?
+     fn
+     : function(something) {
+       return fn.call(this, something);
+     };
 
 const tap = (value, fn) => {
   const curried = (fn) => (
     typeof(fn) === 'function' && fn(value),
     value
   );
- 
-  return fn === 'undefined'
-    ? curried
+
+  return fn === 'undefined' ?
+    curried
     : curried(fn);
 };
 
-'use strict'
+'use strict';
 const maybe = (fun) =>
-  function (args) {
+  function(args) {
     if (args.length === 0) {
-      return
+      return;
     } else {
       for (let arg of args) {
-        if (arg == null) return;
+        if (arg == null) { return; }
       }
-    return fn.apply(this, args)
+      return fn.apply(this, args);
     }
-};
+  };
 
-'use strict'
+'use strict';
 const once = (fn) => {
   let done = false;
- 
-  return function () {
-    return done ? void 0 : ((done = true), fn.apply(this, arguments))
-  }
+
+  return function() {
+    return done ? void 0 : ((done = true), fn.apply(this, arguments));
+  };
 };
 
 const leftVariadic = (fn) => {
   if (fn.length < 1) {
     return fn;
   } else {
-    return function (args) {
-      const gathered = args.slice(0, args.length - fn.length + 1), spread = args.slice(args.length - fn.length + 1);
+    return function(args) {
+      const gathered = args.slice(0, args.length - fn.length + 1);
+      const spread = args.slice(args.length - fn.length + 1);
       return fn.apply(
         this, [gathered].concat(spread)
-      ); }
-    } 
+    ); };
+  }
 };
 
-const leftGather = (outputArrayLength) => { 
-  return function (inputArray) {
-    return [inputArray.slice(0, 
+const leftGather = (outputArrayLength) => {
+  return function(inputArray) {
+    return [inputArray.slice(0,
             inputArray.length - outputArrayLength + 1)]
            .concat(
              inputArray.slice(inputArray.length - outputArrayLength + 1)
-           )
-    } 
+           );
+  };
 };
 
 const compose = (a, b) =>
-(c) => a(b(c))
+(c) => a(b(c));
 
-const pipeline = (...fns) => 
+const pipeline = (...fns) =>
   (value) =>
     fns.reduce((acc, fn) => fn(acc), value);
+
+const mapWith = (fn) =>
+  (list) => list.map(fn);
+
+const flip = (fn) =>
+  function(first, second) {
+    if (arguments.length === 2) {
+      return fn(second, first);
+    } else {
+      return function(second) {
+        return fn(second, first);
+      };
+    };
+  };
+
+const memoized = (fn) => {
+  const lookupTable = {};
+  return function(...args) {
+    const key = JSON.stringify(this, args);
+    return lookupTable[key] || (lookupTable[key] = fn.apply(this, args));
+  };
+};
+
+const getWith = (attr) => (object) => object[attr];
+
+const pluckWith = (attr) => mapWith(getWith(attr));
+
+const deepMapWith = (fn) =>
+  function innerdeepMapWith(tree) {
+    return Array.prototype.map.call(tree, (element) =>
+      Array.isArray(element) ?
+        innerdeepMapWith(element)
+        : fn(element)
+    );
+  };
